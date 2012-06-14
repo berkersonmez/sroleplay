6b45080b6a8a3ef5c1db9b6d5bfc960a0d85ac00
Berker Sonmez <brkrsnmz@gmail.com>
2012-06-14 15:42:40 +0300

    1

--- src/application/server/controller/Ajax.php
+++ src/application/server/controller/Ajax.php
@@ -4,14 +4,17 @@
  * Creates list of messages and returns as JSON.
  * Also decodes JSON with Javascript and creates HTML messages.
  */
-class C_Ajax {
+class C_Ajax
+{
     private $messageList = array();
 
-    public function addMessage($message) {
+    public function addMessage($message)
+    {
         $this->messageList[] = $message;
     }
 
-    private function responseTypeToInt($type) {
+    private function responseTypeToInt($type)
+    {
         if ($type == "error") {
             $type = -1;
         } else if ($type == "success") {
@@ -22,41 +25,54 @@ class C_Ajax {
         return $type;
     }
 
-    private function constructJSON($type, $response) {
+    private function constructJSON($type, $response)
+    {
         return json_encode(array($type => $response));
     }
 
     private function responseJSON($type, $response)
     {
-        //header('Content-type: application/json');
+        header('Content-type: application/json');
         echo $this->constructJSON($this->responseTypeToInt($type), $response);
     }
 
-    public function printResponse($type) {
+    public function printResponse($type)
+    {
         $this->responseJSON($type, $this->messageList);
     }
 
-    public static function jsFormResponseParser() {
+    public static function jsFormResponseParser()
+    {
         ?>
-        var ajaxSuccess = false;
-        function ajaxResponse(data) {
-            var content = "";
-            $.each(data, function(key, val) {
-                if (key == -1) {
-                    content += '<div class="alert alert-error"><ul>';
-                } else if (key == 1) {
-                    ajaxSuccess = true;
-                    content += '<div class="alert alert-success"><ul>';
-                } else if (key == 2) {
-                    content += '<div class="alert"><ul>';
-                }
-                $.each(val, function(key2, val2) {
-                    content+= '<li>'+val2+'</li>';
-                });
-                content+='</ul></div>'
-            });
-            return content;
-        }
+    var ajaxSuccess = false;
+    function ajaxResponse(data) {
+    var content = "";
+    $.each(data, function(key, val) {
+    if (key == -1) {
+    content += '<div class="alert alert-error">
+        <ul>';
+            } else if (key == 1) {
+            ajaxSuccess = true;
+            content += '
+            <div class="alert alert-success">
+                <ul>';
+                    } else if (key == 2) {
+                    content += '
+                    <div class="alert">
+                        <ul>';
+                            }
+                            $.each(val, function(key2, val2) {
+                            content+= '
+                            <li>'+val2+'</li>
+                            ';
+                            });
+                            content+='
+                        </ul>
+                    </div>
+                    '
+                    });
+                    return content;
+                    }
     <?
     }
 
